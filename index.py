e725a03a1feee43ae08f0ffb77b9de70227990a7
Tyler Liu <tyler4long@gmail.com>
2024-08-02 20:38:09 -0700

    Add index2

--- index.py
+++ index.py
@@ -6,7 +6,7 @@ import yfinance
 import polars
 import plotly.express as px
 
-ticker = 'SPY'
+ticker = 'QQQ'
 
 pandas_df = yfinance.download(ticker)
 pandas_df.reset_index(inplace=True)
@@ -21,18 +21,14 @@ df = df.with_columns([
     polars.col('aClose').shift(1).alias('pClose'),
 ])
 df = df.with_columns([
-    ((polars.col('aClose') - polars.col('pClose')) / polars.col('pClose') * 100).alias('change'),
-])
-df = df.with_columns([
-    polars.col('change').fill_null(0),
+    ((polars.col('aClose') - polars.col('pClose')) / polars.col('pClose') * 100).alias('change').fill_null(0),
 ])
 df = df.with_columns([
     (polars.col('date').dt.month().cast(polars.Int16) * 100 + polars.col('date').dt.day()).alias('monthDay'),
 ])
 df = df.group_by('monthDay').agg([
     polars.col('change').mean().alias('meanChange'),
-])
-df = df.sort('monthDay')
+]).sort('monthDay')
 df = df.with_columns([
     polars.col('meanChange').cum_sum().alias('cumulative'),
 ])
@@ -40,6 +36,6 @@ print(df.head())
 print(df.tail())
 df.write_csv('output.csv')
 
-fig = px.line(df, x='monthDay', y='cumulative', title=f'Cumulative Change in {ticker}')
+fig = px.bar(df, x='monthDay', y='cumulative', title=f'Cumulative Change in {ticker}')
 fig.update_layout(xaxis_title='MonthDay', yaxis_title='Profit (%)')
 fig.show()
---
+++ index2.py
@@ -0,0 +1,40 @@
+'''
+统计一只股票在过去所有天的表现, 然后按照日期分组, 看具体到每一天上涨的有多少年, 下跌的有多少年
+'''
+import yfinance
+import polars
+import plotly.express as px
+
+ticker = 'QQQ'
+
+pandas_df = yfinance.download(ticker)
+pandas_df.reset_index(inplace=True)
+df: polars.DataFrame = polars.from_pandas(pandas_df)
+df = df.rename({'Date': 'date', 'Adj Close': 'aClose'})
+df.drop_in_place('Open')
+df.drop_in_place('High')
+df.drop_in_place('Low')
+df.drop_in_place('Close')
+df.drop_in_place('Volume')
+df = df.with_columns([
+    polars.col('aClose').shift(1).alias('pClose').fill_null(polars.col('aClose')),
+])
+df = df.with_columns([
+    polars.when(polars.col('aClose') >= polars.col('pClose'))
+    .then(1)
+    .otherwise(-1)
+    .alias('winLose')
+])
+df = df.with_columns([
+    (polars.col('date').dt.month().cast(polars.Int16) * 100 + polars.col('date').dt.day()).alias('monthDay'),
+])
+df = df.group_by('monthDay').agg([
+    polars.col('winLose').sum().alias('winLose'),
+]).sort('monthDay')
+print(df.head())
+print(df.tail())
+df.write_csv('output2.csv')
+
+fig = px.bar(df, x='monthDay', y='winLose', title=f'Win - Lost times in {ticker}')
+fig.update_layout(xaxis_title='MonthDay', yaxis_title='Win - Lose (times)')
+fig.show()
